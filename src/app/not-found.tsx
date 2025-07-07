c19178b125d2480449773b681f91dc6687bae6c0
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-06 22:58:38 -0300

    Página not found implementada

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+
+export default function NotFoundPage() {
+  return (
+    <div
+      className={clsx(
+        "min-h-[320px] bg bg-slate-900 text-slate-100",
+        "mb-16 p-8",
+        "rounded-2xl",
+        "flex items-center justify-center",
+        "text-center"
+      )}
+    >
+      <div>
+        <h1 className="text-7xl/tight font font-extrabold mb-8">404</h1>
+        <p>Página não encontrada nesse site</p>
+      </div>
+    </div>
+  );
+}
