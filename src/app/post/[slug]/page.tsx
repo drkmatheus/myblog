a24427ca18ac5d8f9551b2420fdef81f2124ac2d
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-07 11:57:35 -0300

    Implementado geração de titulos para paginas com metadata do next

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,9 +1,23 @@
 import { findPostBySlug } from "@/lib/post/queries";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post = await findPostBySlug(slug);
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
