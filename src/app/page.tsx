cecc2cebee05e006571151f5e196e1e0be6d3b51
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-06 22:10:23 -0300

    Pequenas modificações nos metadata do layout e transferencia de componentes footer e header para o layout root da aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
-import { Container } from "@/components/Container";
 import { FeaturedPost } from "@/components/FeaturedPost";
-import { Header } from "@/components/Header";
+
 import { MyLoader } from "@/components/MyLoader";
 import { PostListing } from "@/components/PostsListing";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
-    <Container>
-      <Header />
+    <>
       <FeaturedPost />
       <Suspense fallback={<MyLoader />}>
         <PostListing />
       </Suspense>
-      <footer>
-        <p className="text-5xl font-bold text-center py-7">FOOTER</p>
-      </footer>
-    </Container>
+    </>
   );
 }
