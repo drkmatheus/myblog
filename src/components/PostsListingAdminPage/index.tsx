93cdbcec5bf3d611d621fa1b1db6bdb499a6e38d
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-09 10:50:53 -0300

    Implementação inicial do botão delete para a pagina de listagem de posts da area admin

--- src/components/PostsListingAdminPage/index.tsx
+++ src/components/PostsListingAdminPage/index.tsx
@@ -1,12 +1,42 @@
 import { findAllPosts } from "@/lib/post/queries/admin";
+import clsx from "clsx";
+import { Trash2Icon } from "lucide-react";
+import Link from "next/link";
 
 export async function PostsListingAdminPage() {
   const posts = await findAllPosts();
 
   return (
-    <div className="text-2xl text-center font-bold">
+    <div className="text-1xl text-center font-bold">
       {posts.map((post) => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div
+            className={clsx(
+              "bg-amber-900",
+              "py-2",
+              !post.published && "bg-slate-300",
+              "flex gap-2 items-center justify-between"
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+
+            <button
+              className={clsx(
+                "flex columns-1",
+                "bg-amber-200",
+                "mr-1",
+                "text-red-500",
+                "cursor-pointer",
+                "hover:text-red-700"
+              )}
+              aria-label={`Deletar post ${post.title}`}
+              title="Deletar post"
+            >
+              <Trash2Icon /> Deletar
+            </button>
+          </div>
+        );
       })}
     </div>
   );
