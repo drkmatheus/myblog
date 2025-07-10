7b7e6f9c5b74ba16d097b9652fc8abebfd5d7cf9
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-10 10:22:15 -0300

    Menu admin estilizado para mobile e para desktop

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,16 +1,58 @@
+"use client";
 import clsx from "clsx";
-import { HouseIcon, ScrollTextIcon } from "lucide-react";
+import {
+  CircleXIcon,
+  FilePlus2Icon,
+  HouseIcon,
+  LogOutIcon,
+  MenuIcon,
+  ScrollTextIcon,
+} from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname;
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   const navClasses = clsx(
-    "flex flex-row justify-around bg-sky-600 text-blue-300 p-2 rounded-xl mb-8"
+    "flex flex-col mb-8",
+    "gap-2 px-6 py-2",
+    "sm:flex-row sm:flex-wrap",
+    "bg-sky-600 text-blue-100 rounded-lg",
+    !isOpen && "h-14",
+    !isOpen && "overflow-hidden",
+    "sm:overflow-visible sm:h-auto"
   );
   const linkClasses = clsx(
-    "flex flex-row gap-1 items-center h-10 transition hover:bg-blue-200 rounded-xl p-3 outline"
+    "flex gap-1 justify-start items-center cursor-pointer",
+    "transition hover:bg-blue-700 rounded-lg",
+    "h-10 shrink-0 p-2"
   );
+
+  const closeBtnClasses = clsx(linkClasses, "sm:hidden");
   return (
     <nav className={navClasses}>
+      <button onClick={() => setIsOpen((s) => !s)} className={closeBtnClasses}>
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
       <a className={linkClasses} href="/" target="_blank">
         <HouseIcon />
         Home
@@ -19,6 +61,16 @@ export function MenuAdmin() {
       <Link className={linkClasses} href={"/admin/post"}>
         <ScrollTextIcon />
         Posts
+      </Link>
+
+      <Link className={linkClasses} href={"/admin/post/new"}>
+        <FilePlus2Icon />
+        Criar post
+      </Link>
+
+      <Link className={linkClasses} href={""}>
+        <LogOutIcon />
+        Logout
       </Link>
     </nav>
   );
