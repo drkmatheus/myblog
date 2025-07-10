8f61cb4eb62023c0417b1688cdca6e81078e35c4
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-10 09:23:10 -0300

    Estilização do menu admin com tailwind

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,13 +1,25 @@
+import clsx from "clsx";
+import { HouseIcon, ScrollTextIcon } from "lucide-react";
 import Link from "next/link";
 
 export function MenuAdmin() {
+  const navClasses = clsx(
+    "flex flex-row justify-around bg-sky-600 text-blue-300 p-2 rounded-xl mb-8"
+  );
+  const linkClasses = clsx(
+    "flex flex-row gap-1 items-center h-10 transition hover:bg-blue-200 rounded-xl p-3 outline"
+  );
   return (
-    <nav>
-      <a href="/" target="_blank">
+    <nav className={navClasses}>
+      <a className={linkClasses} href="/" target="_blank">
+        <HouseIcon />
         Home
       </a>
 
-      <Link href={"/admin/post"}>Posts</Link>
+      <Link className={linkClasses} href={"/admin/post"}>
+        <ScrollTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
