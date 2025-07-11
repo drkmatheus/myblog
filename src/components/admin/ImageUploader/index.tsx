bb4ff8c72dd70edac258d5684fd0576d9e5fc01c
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-11 11:31:06 -0300

    Progesso na implementação da funcionalidade de fazer upload de imagens para o servidor

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/Button";
+import { MAX_IMG_SIZE } from "@/lib/post/constants";
 import { UploadIcon } from "lucide-react";
 import { useRef } from "react";
+import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -8,6 +10,31 @@ export function ImageUploader() {
   function handleChooseFile() {
     if (!fileInputRef.current) return;
     fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > MAX_IMG_SIZE) {
+      toast.error("Imagem muito grande. Escolha uma imagem menor que 900KB.");
+
+      fileInput.value = "";
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    // Implementar a action server para fazer o upload do arquivo
+
+    console.log(formData.get("file"));
+
+    fileInput.value = "";
   }
 
   return (
@@ -22,6 +49,7 @@ export function ImageUploader() {
         name="file"
         type="file"
         accept="image/*"
+        onChange={handleChange}
       />
     </div>
   );
--- src/lib/post/constants.ts
+++ src/lib/post/constants.ts
@@ -1,1 +1,2 @@
 export const SIMULATING_LAG = 3000;
+export const MAX_IMG_SIZE = 921600;
