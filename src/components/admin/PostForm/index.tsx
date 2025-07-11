2e8f176c2df4f91dee6c0e4aa1cb56fcde6a7517
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-11 08:56:55 -0300

    Implementado editor de markdown

--- src/components/admin/PostForm/index.tsx
+++ src/components/admin/PostForm/index.tsx
@@ -2,14 +2,25 @@
 import { Button } from "@/components/Button";
 import { InputCheckbox } from "@/components/InputCheckbox";
 import { InputText } from "@/components/InputText";
+import { MarkdownEditorField } from "@/components/MarkdownEditorField";
 import { NewspaperIcon } from "lucide-react";
+import { useState } from "react";
 
 export function PostForm() {
+  const [contentValue, setContentValue] = useState("");
+
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-3">
         <InputText labelText="Digite" placeholder="Digite aqui" />
         <InputText labelText="Digite" placeholder="Digite aqui" />
+        <MarkdownEditorField
+          labelText="Conteúdo"
+          disabled={false}
+          value={contentValue}
+          setValue={setContentValue}
+          textAreaName="content"
+        />
         <InputText disabled labelText="Digite" placeholder="Digite aqui" />
         <Button variant="ghost" size="lg">
           <NewspaperIcon />
