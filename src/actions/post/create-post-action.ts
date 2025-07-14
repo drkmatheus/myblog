ff15b254d85813bcd907cd65eb9d1cf9d8aff015
drkmatheus <matheus.gcrosciati@gmail.com>
2025-07-14 11:14:47 -0300

    Implementado transformação de post to tipo formData para objeto javascript

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -11,10 +11,17 @@ export async function createPostAction(
   prevState: CreatePostActionState,
   formData: FormData
 ): Promise<CreatePostActionState> {
-  const title = formData.get("title")?.toString() || "";
+  if (!(formData instanceof FormData))
+    return {
+      formState: prevState.formState,
+      errors: ["Dados inválidos"],
+    };
+
+  const convertFormToObj = Object.fromEntries(formData.entries());
+  console.log(convertFormToObj);
 
   return {
-    formState: { ...prevState.formState, title },
+    formState: prevState.formState,
     errors: [],
   };
 }
